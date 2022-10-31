56678f55302e0bf2d7db026204d1f54508465e09
Jon <jlinford84@gmail.com>
2022-10-31 11:11:55 +0000

    ticket 3 added planing pictures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <p>words</p>
       </header>
     </div>
   );
