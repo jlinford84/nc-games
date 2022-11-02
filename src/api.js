12834d8a26bac04ab5962fc8f2226a63f7cb5412
Jon <jlinford84@gmail.com>
2022-11-02 11:36:27 +0000

    ticket 7 upvote/downvote on a review working

--- src/api.js
+++ src/api.js
@@ -15,3 +15,18 @@ export const fetchIndividualReview = (review_id) => {
     `https://nc-super-awesome-games.herokuapp.com/api/reviews/${review_id}`
   ).then((res) => res.json());
 };
+
+export const patchReviewVotes = (updatedVotes, review_id) => {
+  return fetch(
+    `https://nc-super-awesome-games.herokuapp.com/api/reviews/${review_id}`,
+    {
+      method: "PATCH",
+      body: JSON.stringify({
+        inc_votes: updatedVotes,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  ).then((response) => response.json());
+};
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -25,8 +25,8 @@ function Category() {
             <li key={review_id}>
               <Link className="rev-link" to={`/reviews/${review_id}`}>
                 <img className="img" alt={`${title}`} src={review_img_url} />
+                <h2>Name: {title}</h2>
               </Link>
-              <h2>Name: {title}</h2>
               <p>Review: {review_body}</p>
             </li>
           );
