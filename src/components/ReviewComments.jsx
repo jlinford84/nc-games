fbc8d0a4db5b4651cbfb60614f06bb51494b6e25
Jon <jlinford84@gmail.com>
2022-11-03 09:16:49 +0000

    ticket 8 view all comments working

---
+++ src/components/ReviewComments.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchComments } from "../api";
+
+function ReviewComments() {
+  const { id } = useParams();
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchComments(id).then(({comment}) => {
+      setComments(comment);
+      setIsLoading(false);
+    });
+  }, [id]);
+
+  if (isLoading) return <p className="loading">Loading...</p>;
+  return (
+    <section className="comments">
+      <ol>
+        {comments.map(({ comment_id, author, created_at, body, votes }) => {
+          return (
+            <li key={comment_id}>
+              <h2>Comment by: {author}</h2>
+              <p>Created on: {created_at}</p>
+              <p>Comment: {body}</p>
+              <p>
+                Votes: {votes}
+              </p>
+            </li>
+          );
+        })}
+      </ol>
+    </section>
+  );
+}
+
+export default ReviewComments;
